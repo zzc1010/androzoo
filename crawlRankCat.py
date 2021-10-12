a822bc9d06c96938cebffe3f5d193fbaeb0e62cc
zzc1010 <zhangzicheng1010@gmail.com>
2021-10-12 10:30:56 +0800

    change chan g

--- crawlRankCat.py
+++ crawlRankCat.py
@@ -24,11 +24,23 @@ if f:
 if DEBUG:
     print catlist
 
+#Read finished list
+finishedList = []
+f = open("Finished.txt", 'r')
+for line in f:
+    finishedCat = lien.rstrip()
+    finishedList.append(finishedCat)
+if f:
+    f.close()
+
 # selenium driver
 browser = webdriver.Firefox()
 
 # loop category list
 for cat in catlist:
+    if cat in finishedList:
+        continue
+    catFile = open("group/"+cat+".txt",'w+')
     i = 1
     has_reach = False
     while True:
@@ -38,37 +50,44 @@ for cat in catlist:
         if DEBUG:
             print url
         browser.get(url)
-    
+
         # wait to load
         time.sleep(10)
-    
+
         # get value
         try:
             tdnodelist = browser.find_elements_by_xpath("//td[@style='text-align:left;']")
             for tdnode in tdnodelist:
                 pkglink = tdnode.find_element_by_tag_name("a")
                 ratingtd = tdnode.find_element_by_xpath("following-sibling::*") \
-                                 .find_element_by_xpath("following-sibling::*")
+                    .find_element_by_xpath("following-sibling::*")
                 installtd = ratingtd.find_element_by_xpath("following-sibling::*")
-    
+
                 pkgsplit = pkglink.get_attribute("href").split("/")
                 if DEBUG:
                     print pkgsplit
                 pkgname = pkgsplit[5]#[0:len(pkgsplit[5])-6]
                 ratingnum = ratingtd.text
                 installnum = installtd.text
-    
+
                 if 'M' not in installnum:
                     has_reach = True
                     break
                 txt = '%s\t%s\t%s\t%s' % (pkgname, cat, ratingnum, installnum)
                 flushPrint(txt)
-    
+                catFile.write(txt+"\n")
+
             if has_reach:
                 break
-    
+
             i = i + 20
         except:
             break
-
+    if catFile:
+        catFile.close()
+    with open("Finished", "w+") as finishedFile:
+        finishedFile.write(cat+"\n")
+    if finishedFile:
+        finishedFile.close()
+        
 browser.quit()
